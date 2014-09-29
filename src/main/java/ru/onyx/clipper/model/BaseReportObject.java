80e14c4bb12f56fa34fd2803f5d08d13ca58c0e8
anton <Testarossa512>
2014-09-29 12:59:57 +0400

    Added some method comments. Fixed missing property text at <cell> when there is no text inside tag. Fixed possible NullPointerException at ReportStrUtils.replaceDecSeparator method.

--- src/main/java/ru/onyx/clipper/model/BaseReportObject.java
+++ src/main/java/ru/onyx/clipper/model/BaseReportObject.java
@@ -1309,6 +1309,12 @@ public abstract class BaseReportObject {
         return ret;
     }
 
+
+	/**
+	 * Конвертирует propertyValue в указанный в параметре format формат даты.
+	 * @param propertyValue строковое значение с датой.
+	 * @return отформатированное строковое значение с датой.
+	 */
     protected String ConvertPropertyToSpecificDateFormat(String propertyValue) {
 
         Date s = propertyGetter.GetPropertyStringAsDate(propertyValue, getDateFormat());
--- src/main/java/ru/onyx/clipper/utils/ReportStrUtils.java
+++ src/main/java/ru/onyx/clipper/utils/ReportStrUtils.java
@@ -10,26 +10,36 @@ import java.util.Locale;
  */
 public class ReportStrUtils {
 
+	/**
+	 * Заменяет десятчиный разделитель '.' на указанный и добавляет символы '00' к строке.
+	 * @param inputString число с десятичным разделителем '.'
+	 * @param separator новый символ разделителя.
+	 * @return число с новым десятичным разделителем.
+	 */
     public static String replaceDecSeparator(String inputString, String separator){
-
-        if(inputString.equals("")) return inputString;
-
         if(inputString!=null) {
-            DecimalFormatSymbols otherSymbols = new DecimalFormatSymbols(Locale.US);
-            otherSymbols.setDecimalSeparator(separator.charAt(0));
-            DecimalFormat df = new DecimalFormat("#.00", otherSymbols);
-            return df.format(Float.parseFloat(inputString));
+			if(inputString.equals("")) {
+				return inputString;
+			}
+			else{
+				DecimalFormatSymbols symbols = new DecimalFormatSymbols(Locale.US);
+				symbols.setDecimalSeparator(separator.charAt(0));
+				DecimalFormat df = new DecimalFormat("#.00", symbols);
+				return df.format(Float.parseFloat(inputString));
+			}
         }
-
-
         return null;
     }
 
+	/**
+	 * Обрамляет отрицательное значение скобками. Знак '-' при этом не выводится.
+	 * @param inputString отрицательное значение со знаком '-' (строкове)
+	 * @return модуль отрицательного значения в скобках (строковое) или само значение, если оно положительно
+	 */
     public static String embraceNegativeValue(String inputString){
         if ( inputString.matches("[0-9-]*") ) {
             if (Double.parseDouble(inputString) >= 0)
                 return inputString;
-
             inputString = inputString.replace('-', '(');
             inputString = inputString+')';
         }
