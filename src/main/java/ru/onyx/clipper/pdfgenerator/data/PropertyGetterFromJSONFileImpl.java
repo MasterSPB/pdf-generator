6e8de3205ccf5d9576cbcc353ea2c421da187ee1
ViktorZahar <viktorzahar@gmail.com>
2015-04-23 14:31:12 +0300

    Fix single element bad replacement

--- src/main/java/ru/onyx/clipper/pdfgenerator/data/PropertyGetterFromJSONFileImpl.java
+++ src/main/java/ru/onyx/clipper/pdfgenerator/data/PropertyGetterFromJSONFileImpl.java
@@ -45,8 +45,8 @@ public class PropertyGetterFromJSONFileImpl implements PropertyGetter{
             e.printStackTrace();
         } catch (PathNotFoundException e2) {
 			if(jsonPath.contains("[0]")){
-				int firstBracketIndex = jsonPath.lastIndexOf("[");
-				int lastBracketIndex = jsonPath.lastIndexOf("]");
+				int firstBracketIndex = jsonPath.lastIndexOf("[0");
+				int lastBracketIndex = jsonPath.lastIndexOf("0]")+1;
 				StringBuilder temp = new StringBuilder(jsonPath.substring(0,firstBracketIndex));
 				temp.append(jsonPath.substring(lastBracketIndex + 1, jsonPath.length()));
 				return GetProperty(temp.toString());
